b19c72412deb31d3421274d98873259c8acae872
artemkoorochka <artem@koorochka.com>
2020-01-08 15:55:27 +0200

    Static info

---
+++ akcii-i-skidki/index.php
@@ -0,0 +1,95 @@
+<?
+
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+
+$APPLICATION->SetTitle("Акции и скидки на лечение алкоголизма. Лазарет");
+
+?><h2>Постоянные скидки <sup>1</sup></h2>
+<a name="5procentov"></a>
+<p>
+ <span style="font-size: 24pt; background-color: #99cc00; color: #ffffff; padding: 10px;">-5%</span>
+</p>
+<p>
+	 скидка при&nbsp;оформлении заявки с&nbsp;сайта<sup><br>
+ </sup>скидка на&nbsp;повторную<span style="margin-right: 0.3em;"> </span><span style="margin-right: 0.3em;"> </span> <span style="margin-left: -0.3em;"><span style="margin-right: 0.3em;"> </span> <span style="margin-left: -0.3em;"><span style="margin-left: -0.3em;">«запретительную</span></span></span>» процедуру <sup>2</sup><br>
+на размещение при повторной госпитализации
+</p>
+<hr>
+ <a name="10procentov"></a>
+<p>
+ <span style="font-size: 24pt; background-color: #99cc00; color: #ffffff; padding: 10px;">-10%</span>
+</p>
+<p>
+	 скидка на&nbsp;повторный вызов врача на&nbsp;дом<br>
+	 скидка на&nbsp;повторную амбулаторную детоксикацию<br>
+	 скидка на&nbsp;повторную детоксикацию в&nbsp;стационаре <sup>3</sup><br>
+cкидка на своевременное продление кодирования
+</p>
+<hr>
+<p>
+ <span style="font-size: 24pt; background-color: #99cc00; color: #ffffff; padding: 10px;">-20%</span>
+</p>
+<p>
+	 скидка по&nbsp;программе<span style="margin-right: 0.3em;"> </span><span style="margin-right: 0.3em;"> </span> <span style="margin-left: -0.3em;"><span style="margin-right: 0.3em;"> </span> <span style="margin-left: -0.3em;"><span style="margin-left: -0.3em;">«Вдвоем</span></span></span> выгоднее» <sup>3</sup><br>
+</p>
+<hr>
+ <a name="taxi"></a>
+<p>
+ <span style="font-size: 24pt; background-color: #99cc00; color: #ffffff; padding: 10px;">-100%</span>
+</p>
+<p>
+	 возврат стоимости такси до<span style="margin-right: 0.3em;"> </span><span style="margin-right: 0.3em;"> </span> <span style="margin-left: -0.3em;"><span style="margin-right: 0.3em;"> </span> <span style="margin-left: -0.3em;"><span style="margin-left: -0.3em;">«Лазарета</span></span></span>» в&nbsp;черте города<span style="margin-right: 0.3em;"> </span> <span style="margin-left: -0.3em;"><span style="margin-right: 0.3em;"> </span> <span style="margin-left: -0.3em;"><span style="margin-right: 0.3em;"> </span> <span style="margin-left: -0.3em;">(при</span></span></span> условии пребывания в&nbsp;стационаре не&nbsp;менее 3-х суток)
+</p>
+<hr><a name="vivitrol"></a>
+<p>
+ <span style="font-size: 24pt; background-color: #99cc00; color: #ffffff; padding: 10px;">-1000 р.</span>
+</p>
+<p>
+	 при соблюдении сроков инъекций каждая последующая инъекция препарата<span style="margin-right: 0.3em;"> </span><span style="margin-right: 0.3em;"> </span> <span style="margin-left: -0.3em;"><span style="margin-right: 0.3em;"> </span> <span style="margin-left: -0.3em;"><span style="margin-left: -0.3em;">«Вивитрол</span></span></span>®» в&nbsp;стандартном курсе дешевле на&nbsp;1000 рублей:<br>
+ <span style="font-size: 18pt;">27000&nbsp;— 26000&nbsp;— 25000&nbsp;— 24000&nbsp;— 23000&nbsp;— 22000 р.</span>
+</p>
+<hr>
+<p>
+ <sup>1</sup>&nbsp;<em>— скидки и&nbsp;акции не&nbsp;суммируются</em>
+</p>
+<p>
+ <sup>2</sup> <em>— при&nbsp;условии обращения до&nbsp;даты контрольной явки</em>
+</p>
+<p>
+ <sup>3</sup>&nbsp;&nbsp;<em>— распространяется на стоимость размещения не менее 3-х суток и базовую терапию</em>
+</p>
+<p>
+	 &nbsp;
+</p>
+<hr>
+<p style="margin-top: 30px;">
+ <span style="font-size: 24pt; background-color: #3b9ef3; color: #ffffff; padding: 10px;"> «Курсом &nbsp;— еще&nbsp;дешевле!» </span>
+</p>
+<p>
+ <em>акция действует при&nbsp;единовременной оплате <em style="box-sizing: border-box;">амбулаторного&nbsp;</em>курса</em>
+</p>
+<hr><a name="plazmafarez"></a>
+<p>
+	 курс из&nbsp;<span style="font-size: 14pt;">3-х</span> сеансов плазмафереза&nbsp;— <span style="font-size: 14pt;"><span style="font-size: 18pt;">7 800</span> руб.</span>
+</p>
+<hr>
+<p>
+	 курс из&nbsp;<span style="font-size: 14pt;">6-ти</span> сеансов озонотерапии&nbsp;— <span style="font-size: 14pt;"><span style="font-size: 18pt;">11 200</span> руб.</span>
+</p>
+<hr>
+<p>
+	 курс из&nbsp;<span style="font-size: 14pt;">5-ти</span> гепатопротективных капельниц – <span style="font-size: 14pt;"><span style="font-size: 18pt;">8 900</span> руб.</span>
+</p>
+<hr>
+<p>
+	 курс из&nbsp;<span style="font-size: 14pt;">6-ти</span> церебропротективных капельниц – <span style="font-size: 14pt;"><span style="font-size: 18pt;">10 000</span> руб.</span>
+</p>
+<hr>
+<p>
+ <span style="font-size: 24pt; background-color: #99cc00; color: #ffffff; padding: 10px;">6 по цене 4-х</span>
+</p>
+<p>
+	 курс из&nbsp;<span style="font-size: 14pt;">6-ти</span> сеансов любых аппаратных процедур по&nbsp;цене <span style="font-size: 14pt;">4-х</span><span style="margin-right: 0.3em;"> </span> <span style="margin-left: -0.3em;"><span style="margin-right: 0.3em;"> </span> <span style="margin-left: -0.3em;"><span style="margin-right: 0.3em;"> </span> <br>
+ <span style="margin-left: -0.3em;">(электросон</span></span></span>, электропсихотерапия, аудиовизуальная психотерапия и&nbsp;т.д.)
+</p>
+<br><?require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');?>
